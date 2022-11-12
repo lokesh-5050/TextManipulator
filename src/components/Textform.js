caa9b8281d1d1264f6a995af89a43d0da51c4b2b
lokesh-mali <mali.lokesh5050@gmail.com>
2022-11-12 23:03:18 +0530

    copy

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,20 +1,52 @@
-import React , {useState} from "react";
-
+import React, { useState } from "react";
 
 export default function Textform(props) {
-  const [text , setText] = useState("Enter text here");
+  const [text, setText] = useState("Enter text here");
+  const toUpperCase = () => {
+    console.log(text);
+    setText(text.toUpperCase());
+  };
+  const toLowerCase = () => {
+    console.log(text);
+    setText(text.toLowerCase());
+  };
+  const copyToClip = (e) => {
+    console.log(e);
+    // console.log(e.target.parentElement.parentElement.children[1].defaultValue)
+    let copyAllText =
+      e.target.parentElement.parentElement.children[1].defaultValue;
+    navigator.clipboard.writeText(copyAllText);
+  };
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setText(e.target.value);
+  };
+  const removeAll = () =>{
+    setText("")
+  }
   return (
     <div style={{ width: "70%", marginTop: "3vw" }} className="container">
       <div className="mb-3 ">
         <h1>{props.heading}</h1>
 
-        <textarea className="form-control mt-5" id="myBox" rows="8"></textarea>
-        <div style={{display:"flex" , gap:"2vw"}} className="btns">
-        <button className="btn btn-primary mt-4">Convert To UpperCase</button>
-        <button className="btn btn-primary mt-4">Convert To lowerCase</button>
-        <button className="btn btn-primary mt-4">Copy All Text</button>
-        <button className="btn btn-primary mt-4">Remove All</button>
-
+        <textarea
+          className="form-control mt-5"
+          id="myBox"
+          rows="8"
+          value={text}
+          onChange={handleChange}
+        ></textarea>
+        <div style={{ display: "flex", gap: "2vw" }} className="btns">
+          <button className="btn btn-primary mt-4" onClick={toUpperCase}>
+            Convert To UpperCase
+          </button>
+          <button className="btn btn-primary mt-4" onClick={toLowerCase}>
+            Convert To lowerCase
+          </button>
+          <button className="btn btn-primary mt-4" onClick={copyToClip}>
+            Copy All Text
+          </button>
+          <button className="btn btn-primary mt-4" onClick={removeAll}>Remove All</button>
         </div>
       </div>
     </div>
