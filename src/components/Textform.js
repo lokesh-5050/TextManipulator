a8ec63e72a16f1173540b4bb38b4127b04b9788a
lokesh-mali <mali.lokesh5050@gmail.com>
2022-11-13 08:00:48 +0530

    time

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("Enter texts here");
+  const [times , setTime] = useState("Time Now")
+  // const removeWords = () =>{
+  //   let textAreaWords = ""
+  //   setText(textAreaWords)
+  // }
+ 
+  
   const toUpperCase = () => {
     console.log(text);
     setText(text.toUpperCase());
@@ -13,27 +20,35 @@ export default function Textform(props) {
   const copyToClip = (e) => {
     console.log(e);
     // console.log(e.target.parentElement.parentElement.children[1].defaultValue)
-    let copyAllText =
-      e.target.parentElement.parentElement.children[1].defaultValue;
+    let copyAllText = e.target.parentElement.parentElement.children[2].value;
+    console.log(copyAllText)
     navigator.clipboard.writeText(copyAllText);
   };
   const handleChange = (e) => {
     // console.log(e.target.value);
+
     setText(e.target.value);
   };
-  const removeAll = () =>{
-    setText("")
+  const removeAll = () => {
+    setText("");
+  };
+  const time = ()=>{
+    let today = new Date().toLocaleTimeString()
+    setTime(today)
   }
+  setInterval(time , 1000)
   return (
     <div style={{ width: "70%", marginTop: "3vw" }} className="container">
       <div className="mb-3 ">
         <h1>{props.heading}</h1>
-
+        <div className="time" onLoad={time}>{times}</div>
         <textarea
           className="form-control mt-5"
           id="myBox"
           rows="8"
           value={text}
+          // o  nFocus={removeWords}
+          
           onChange={handleChange}
         ></textarea>
         <div style={{ display: "flex", gap: "2vw" }} className="btns">
@@ -46,7 +61,9 @@ export default function Textform(props) {
           <button className="btn btn-primary mt-4" onClick={copyToClip}>
             Copy All Text
           </button>
-          <button className="btn btn-primary mt-4" onClick={removeAll}>Remove All</button>
+          <button className="btn btn-primary mt-4" onClick={removeAll}>
+            Remove All
+          </button>
         </div>
       </div>
     </div>
