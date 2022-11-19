e3075a2635857f9a385bbfca9bfd12dca8492f6e
Lokesh-mali <102310338+lokesh-5050@users.noreply.github.com>
2022-11-19 09:51:50 +0000

    DarkMode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React , {useState} from 'react'
 import '../src/styles/App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
 function App() {
+  const [mode , setMode] = useState('dark')
+
   return (
     <>
-    <Navbar title = "LokeshApp" aboutText="About Us" ></Navbar>
+    <Navbar title = "LokeshApp" aboutText="About Us" mode={mode} ></Navbar>
     <Textform heading="Enter texts to analyize"></Textform>
     
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className={`navbar navbar-expand-${props.mode} bg-${props.mode}`} style={{color:props.mode === 'dark'?'white':'#333'}}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/ ">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <a className="navbar-brand"  style={{color:props.mode === 'dark'?'white':'#333'}}>{props.title}</a>
+    <button className="navbar-toggler-text-light"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" ></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/ ">Home</a>
+          <a className="nav-link active" aria-current="page" style={{color:props.mode === 'dark'?'white':'#333'}}>Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/ ">{props.aboutText}</a>
+          <a className="nav-link active" aria-current="page" style={{color:props.mode === 'dark'?'white':'#333'}}>{props.aboutText}</a>
         </li>
         
       </ul>
